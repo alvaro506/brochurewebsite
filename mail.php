8e1726adcd00767a75b6c032bfc987403132eae2
lincgr <lincgr@gmail.com>
2017-10-23 12:58:01 -0600

    Change Email

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "info@yourdomain.com";
+        $recipient = "info@saramancuso.fit";
 
         // Set the email subject.
         $subject = "New contact from $name";
